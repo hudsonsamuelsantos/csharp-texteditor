d80032802df28bc88cab887532c4e0cda49a8d45
Hudson Santos <hudsnss@gmail.com>
2025-01-23 21:44:11 -0300

    project init

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Bem vindo ao seu Editor de Textos! Digite o numéro referente a opção desejada.");
+    Console.WriteLine("1 - Abrir");
+    Console.WriteLine("2 - Editar");
+    Console.WriteLine("0 - Sair");
+
+    string option = Console.ReadLine();
+    short optionNumber = 0;
+
+    try
+    {
+        optionNumber = short.Parse(option);
+    }
+    catch
+    {
+        Console.Clear();
+        for (int i = 5; i > 0; i--)
+        {
+            Console.WriteLine($"Parece que você não digitou um número! Tente novamente em {i} segundos!");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+        Menu();
+    };
+
+    switch (optionNumber)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default:
+            Console.Clear();
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine($"Erro na opção digitada! Tente novamente em {i} segundos!");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+            Menu();
+            break;
+    };
+
+    static void Open()
+    {
+        Console.WriteLine("Abriu!");
+    };
+
+    static void Edit()
+    {
+        Console.WriteLine("Editou!");
+    };
+}
+
+Menu();
